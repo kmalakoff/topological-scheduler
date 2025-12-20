51d87b302bd3e5fc61c74c9c41c40ff272b4a511
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-20 12:37:17 -0800

    Migrated to void and simplified compat layer

--- src/index.ts
+++ src/index.ts
@@ -11,23 +11,15 @@ function topologicalScheduler<T, R>(graph: DependencyGraph<T>, worker: WorkerFun
 
 // Implementation
 function topologicalScheduler<T, R>(graph: DependencyGraph<T>, worker: WorkerFunction<T, R>, options?: SchedulerOptions | SchedulerCallback<T, R>, callback?: SchedulerCallback<T, R>): void | Promise<SchedulerResult<T, R>[]> {
-  // Normalize arguments
-  if (typeof options === 'function') {
-    callback = options;
-    options = {};
-  }
-  options = options || {};
+  callback = typeof options === 'function' ? options : callback;
+  options = typeof options === 'function' ? {} : ((options || {}) as SchedulerOptions);
 
-  // Callback mode
   if (typeof callback === 'function') return schedule(graph, worker, options, callback);
-
-  // Promise mode
-  return new Promise((resolve, reject) => {
-    schedule(graph, worker, options as SchedulerOptions, (err, results) => {
-      if (err) return reject(err);
-      resolve(results);
-    });
-  });
+  return new Promise((resolve, reject) =>
+    schedule(graph, worker, options, (err, results) => {
+      err ? reject(err) : resolve(results);
+    })
+  );
 }
 
 export default topologicalScheduler;
