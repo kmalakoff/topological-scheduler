6a80f0ad7c3c26df721a6c9a09e55d1f60982f66
Kevin Malakoff <kmalakoff@gmail.com>
2025-12-26 19:17:24 -0800

    Simplify code formatting in index.ts and compat.ts

--- test/lib/compat.ts
+++ test/lib/compat.ts
@@ -1,8 +1,6 @@
 // Array.prototype.find (ES2015)
 export function arrayFind<T>(arr: T[], predicate: (item: T, index: number, arr: T[]) => boolean): T | undefined {
-  if (typeof arr.find === 'function') {
-    return arr.find(predicate);
-  }
+  if (typeof arr.find === 'function') return arr.find(predicate);
   for (let i = 0; i < arr.length; i++) {
     if (predicate(arr[i], i, arr)) return arr[i];
   }
@@ -11,8 +9,6 @@ export function arrayFind<T>(arr: T[], predicate: (item: T, index: number, arr: 
 
 // Array.prototype.includes (ES2016)
 export function arrayIncludes<T>(arr: T[], value: T): boolean {
-  if (typeof arr.includes === 'function') {
-    return arr.includes(value);
-  }
+  if (typeof arr.includes === 'function') return arr.includes(value);
   return arr.indexOf(value) !== -1;
 }
